959d4fac765077a7b8d0b9a2e5d591339bac0258
Kashvi65 <kashvi.cs23@bmsce.ac.in>
2025-03-11 10:14:28 +0530

    Create gasstation.c

---
+++ gasstation.c
@@ -0,0 +1,16 @@
+int canCompleteCircuit(int* gas, int gasSize, int* cost, int costSize){
+    int gas_tank = 0, start_index = 0, sum = 0;
+        
+        for (int i=0; i<costSize; i++) {
+            
+            sum += gas[i] - cost[i];
+            gas_tank += gas[i] - cost[i];
+            
+            if (gas_tank < 0) {
+                start_index = i+1;
+                gas_tank = 0;
+            }
+            
+        }
+        return sum < 0 ? -1 : start_index;
+}
